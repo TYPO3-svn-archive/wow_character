afc329745a13f6b74ef960d1a13216c5666237b8
jobe <jobe@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-05-09 07:47:41 +0000

    unify category & dependencies
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/wow_character/trunk@33057 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext: "wow_character"
 #
-# Auto generated 15-01-2009 17:06
+# Auto generated 14-09-2009 13:32
 #
 # Manual updates:
 # Only the data in the array - anything else is removed by next write.
@@ -11,12 +11,12 @@
 ########################################################################
 
 $EM_CONF[$_EXTKEY] = array(
-	'title' => 'World of Warcraft - Character Display',
+	'title' => 'World of Warcraft - Character',
 	'description' => 'Displays a character from World of Warcraft',
-	'category' => 'plugin',
+	'category' => 'World of Warcraft',
 	'shy' => 0,
-	'version' => '0.1.2',
-	'dependencies' => '',
+	'version' => '0.1.3',
+	'dependencies' => 'cms',
 	'conflicts' => '',
 	'priority' => '',
 	'loadOrder' => '',
@@ -35,14 +35,18 @@ $EM_CONF[$_EXTKEY] = array(
 	'constraints' => array(
 		'depends' => array(
 			'cms' => '',
+			'typo3' => '4.3.2',
 			'php' => '5.0',
+			'wow_armory' => '0.0.0',
 		),
 		'conflicts' => array(
 		),
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:71:{s:9:"ChangeLog";s:4:"bc6a";s:10:"README.txt";s:4:"a36b";s:9:"Thumbs.db";s:4:"3455";s:12:"ext_icon.gif";s:4:"79f2";s:17:"ext_localconf.php";s:4:"8a5e";s:14:"ext_tables.inc";s:4:"f8cb";s:14:"ext_tables.php";s:4:"d05d";s:14:"ext_tables.sql";s:4:"8e1d";s:28:"ext_typoscript_constants.txt";s:4:"d3c4";s:24:"ext_typoscript_setup.txt";s:4:"a551";s:19:"flexform_ds_pi1.xml";s:4:"1f55";s:19:"flexform_ds_pi2.xml";s:4:"5116";s:35:"icon_tx_wowcharacter_characters.gif";s:4:"fafd";s:9:"index.php";s:4:"73ec";s:13:"locallang.xml";s:4:"f630";s:16:"locallang.xml";s:4:"7054";s:7:"tca.php";s:4:"d7d8";s:17:"wow_character.dsk";s:4:"bb5c";s:17:"wow_character.ppj";s:4:"22f4";s:17:"wow_character.ppx";s:4:"238b";s:10:"x-info.php";s:4:"f7dc";s:14:"pi2/ce_wiz.gif";s:4:"3ff0";s:33:"pi2/class.tx_wowcharacter_pi2.php";s:4:"f4cf";s:41:"pi2/class.tx_wowcharacter_pi2_wizicon.php";s:4:"d49f";s:13:"pi2/clear.gif";s:4:"cc11";s:13:"pi2/index.php";s:4:"73ec";s:17:"pi2/locallang.xml";s:4:"f826";s:17:"pi2/translate.php";s:4:"f8e7";s:27:"pi2/tx_wowcharacter_pi2.css";s:4:"6975";s:28:"pi2/tx_wowcharacter_pi2.html";s:4:"809b";s:24:"pi2/static/editorcfg.txt";s:4:"6124";s:24:"pi1/background-mask1.png";s:4:"550d";s:24:"pi1/background-mask2.png";s:4:"0ad8";s:18:"pi1/background.png";s:4:"dbd2";s:14:"pi1/ce_wiz.gif";s:4:"3ff0";s:33:"pi1/class.tx_wowcharacter_pi1.php";s:4:"591e";s:43:"pi1/class.tx_wowcharacter_pi1_character.php";s:4:"fb71";s:41:"pi1/class.tx_wowcharacter_pi1_wizicon.php";s:4:"a220";s:13:"pi1/clear.gif";s:4:"cc11";s:12:"pi1/icon.png";s:4:"552e";s:13:"pi1/index.php";s:4:"73ec";s:17:"pi1/locallang.xml";s:4:"772a";s:14:"pi1/master.css";s:4:"336c";s:14:"pi1/overlib.js";s:4:"91da";s:12:"pi1/test.php";s:4:"8af8";s:12:"pi1/test.xml";s:4:"75f8";s:17:"pi1/translate.php";s:4:"f8e7";s:14:"pi1/ttip-b.png";s:4:"166b";s:15:"pi1/ttip-bl.png";s:4:"a461";s:15:"pi1/ttip-br.png";s:4:"0ad3";s:14:"pi1/ttip-l.png";s:4:"7034";s:14:"pi1/ttip-r.png";s:4:"3f6a";s:14:"pi1/ttip-t.png";s:4:"4a0c";s:15:"pi1/ttip-tl.png";s:4:"ca08";s:15:"pi1/ttip-tr.png";s:4:"36a8";s:14:"pi1/ttipbg.png";s:4:"4c3c";s:27:"pi1/tx_wowcharacter_pi1.css";s:4:"3c39";s:28:"pi1/tx_wowcharacter_pi1.html";s:4:"61ca";s:15:"pi1/xlstest.php";s:4:"edb4";s:24:"pi1/static/editorcfg.txt";s:4:"56ee";s:24:"pi1/static/css/setup.txt";s:4:"1079";s:29:"doc/TYPO3 Class Reference.URL";s:4:"5602";s:31:"doc/TYPO3 Coding Guidelines.URL";s:4:"d767";s:53:"doc/TYPO3Wiki - Extension Development (Flexforms).URL";s:4:"b75b";s:41:"doc/TYPO3Wiki - Extension Development.URL";s:4:"bbbb";s:27:"doc/WoWWiki - Icon List.URL";s:4:"843e";s:15:"doc/WoWWiki.URL";s:4:"e66a";s:24:"doc/armory - strings.URL";s:4:"618a";s:14:"doc/manual.sxw";s:4:"98c9";s:19:"doc/wizard_form.dat";s:4:"630c";s:20:"doc/wizard_form.html";s:4:"4365";}',
+	'_md5_values_when_last_written' => 'a:52:{s:12:"ext_icon.gif";s:4:"79f2";s:17:"ext_localconf.php";s:4:"3f65";s:14:"ext_tables.php";s:4:"9a92";s:14:"ext_tables.sql";s:4:"2c71";s:28:"ext_typoscript_constants.txt";s:4:"d41d";s:24:"ext_typoscript_setup.txt";s:4:"d41d";s:35:"icon_tx_wowcharacter_characters.gif";s:4:"fafd";s:9:"index.php";s:4:"73ec";s:13:"locallang.xml";s:4:"4475";s:7:"tca.php";s:4:"e9bb";s:17:"wow_character.kpf";s:4:"ec4e";s:13:"doc/Thumbs.db";s:4:"6a25";s:14:"doc/manual.sxw";s:4:"4d86";s:19:"doc/wizard_form.dat";s:4:"630c";s:20:"doc/wizard_form.html";s:4:"4365";s:39:"inc/class.tx_wowcharacter_character.php";s:4:"5ac3";s:43:"inc/class.tx_wowcharacter_itemsProcFunc.php";s:4:"64bb";s:26:"mmlib/class.mmlib_base.php";s:4:"4983";s:24:"mmlib/class.mmlib_db.php";s:4:"bbf6";s:36:"mmlib/class.mmlib_mvc_controller.php";s:4:"4e5b";s:31:"mmlib/class.mmlib_mvc_model.php";s:4:"c0a6";s:30:"mmlib/class.mmlib_mvc_view.php";s:4:"3313";s:28:"mmlib/class.mmlib_object.php";s:4:"059f";s:28:"mmlib/class.mmlib_pibase.php";s:4:"0024";s:25:"mmlib/class.mmlib_xml.php";s:4:"a7e0";s:14:"pi1/ce_wiz.gif";s:4:"3ff0";s:33:"pi1/class.tx_wowcharacter_pi1.php";s:4:"596e";s:41:"pi1/class.tx_wowcharacter_pi1_wizicon.php";s:4:"a220";s:16:"pi1/flexform.xml";s:4:"3783";s:13:"pi1/index.php";s:4:"73ec";s:17:"pi1/locallang.xml";s:4:"248d";s:17:"pi1/translate.php";s:4:"f8e7";s:27:"pi1/tx_wowcharacter_pi1.css";s:4:"f490";s:28:"pi1/tx_wowcharacter_pi1.html";s:4:"bc7b";s:33:"pi1/tx_wowcharacter_pi1_table.css";s:4:"46f8";s:34:"pi1/tx_wowcharacter_pi1_table.html";s:4:"394e";s:28:"pi1/res/background-mask1.png";s:4:"550d";s:28:"pi1/res/background-mask2.png";s:4:"0ad8";s:22:"pi1/res/background.png";s:4:"dbd2";s:24:"pi1/static/constants.txt";s:4:"4b2f";s:20:"pi1/static/setup.txt";s:4:"e120";s:14:"pi2/ce_wiz.gif";s:4:"3ff0";s:33:"pi2/class.tx_wowcharacter_pi2.php";s:4:"7e38";s:41:"pi2/class.tx_wowcharacter_pi2_wizicon.php";s:4:"d49f";s:16:"pi2/flexform.xml";s:4:"9f59";s:13:"pi2/index.php";s:4:"73ec";s:17:"pi2/locallang.xml";s:4:"f826";s:17:"pi2/translate.php";s:4:"f8e7";s:27:"pi2/tx_wowcharacter_pi2.css";s:4:"6975";s:28:"pi2/tx_wowcharacter_pi2.html";s:4:"0d43";s:24:"pi2/static/constants.txt";s:4:"b235";s:20:"pi2/static/setup.txt";s:4:"8523";}',
+	'suggests' => array(
+	),
 );
 
 ?>
